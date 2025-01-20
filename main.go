5188bf3d79c3dcf7298745b9e59b16a616ecf907
Noah <fg5678910@yahoo.com.tw>
2025-01-20 18:08:35 +0800

    feat: 10 locate fixed rune test ok

--- main.go
+++ main.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(isMatch("aa", "a"))
+	fmt.Println(isMatch("abcccaa", "ab*ccca.*a"))
 }
--- problem10.go
+++ problem10.go
@@ -2,159 +2,83 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func isMatch(s string, p string) bool {
-	fmt.Println(s)
-	fmt.Println(p)
-	pIndex, sIndex := len(p)-1, len(s)-1
-
-	// comparing pattern
-	for pIndex >= 0 {
-		fmt.Println("sIndex:", sIndex, "pIndex:", pIndex)
-
-		checkIndex := sIndex
-
-		// if is flexible
-		if p[pIndex] == '*' {
-			for p[pIndex] == '*' {
-				pIndex--
-			}
-			for p[pIndex] == '.' || s[sIndex] == p[pIndex] {
-				sIndex--
-				// if different, then break
-				if sIndex == -1 {
-					break
-				}
-			}
-			pIndex--
-		} else {
-			if p[pIndex] != '.' && s[sIndex] != p[pIndex] {
-				return false
-			}
-			pIndex--
-			sIndex--
+	checkQueue := []string{}
+	i := len(p) - 1
+	for i >= 0 {
+		// skip rune with '*'
+		if p[i] == '*' {
+			i -= 2
+			continue
 		}
 
-		// if have compared all s, but p haven't
-		if sIndex == -1 && pIndex != -1 {
-			return isMatch(s[:checkIndex+1], p[:pIndex+1]+"*")
+		for j := i; j >= 0; j-- {
+			if j == 0 {
+				checkQueue = append([]string{p[j : i+1]}, checkQueue...)
+				i--
+				break
+			}
+			if p[j] != p[j-1] {
+				checkQueue = append([]string{p[j : i+1]}, checkQueue...)
+				i -= (i - j + 1)
+				break
+			}
 		}
 	}
 
-	return sIndex == -1
+	for _, section := range checkQueue {
+		fmt.Println(section)
+	}
+
+	fmt.Printf("s: %v\n", s)
+	for i := 0; i < len(checkQueue)-1; i++ {
+		fmt.Println("checkQueue[i]: ", checkQueue[i], "checkQueue[i+1]: ", checkQueue[i+1])
+
+		index1 := indexOfNum(s, checkQueue[i], counts(checkQueue[:i], checkQueue[i])+1)
+		index2 := indexOfNum(s, checkQueue[i+1], counts(checkQueue[:i+1], checkQueue[i+1])+1)
+
+		fmt.Println("index1: ", index1, "index2: ", index2)
+		if index1 == -1 || index2 == -1 {
+			return false
+		}
+		if index1 > index2 {
+			return false
+		}
+	}
+
+	return true
 }
 
-// func isMatch(s string, p string) bool {
-// 	fmt.Println(s)
-// 	fmt.Println(p)
-// 	pIndex, sIndex, isMatch, isMultiple := 0, 0, true, false
+// indexOfNum returns the index of the num-th occurrence of substr in s
+func indexOfNum(s, substr string, num int) (index int) {
+	index = strings.Index(s, substr)
+	// end point
+	if num == 1 {
+		return
+	}
+	// if the length of s is less than the length of substr, return -1
+	if len(s) < len(substr) {
+		return -1
+	}
+	// recursive call
+	nextIndex := indexOfNum(s[strings.Index(s, substr)+len(substr):], substr, num-1)
+	// if the next index is -1, return -1
+	if nextIndex == -1 {
+		return -1
+	}
+	// add the length of substr and the next index
+	index += (len(substr) + nextIndex)
 
-// 	// comparing pattern
-// 	for pIndex < len(p) {
-// 		fmt.Println("sIndex:", sIndex, "pIndex:", pIndex)
+	return
+}
 
-// 		if pIndex == len(p)-1 {
-// 			isMultiple = false
-// 		} else {
-// 			isMultiple = p[pIndex+1] == '*'
-// 		}
-
-// 		// check if have duplicate rune, * change with last duplicate rune
-// 		if isMultiple && pIndex < len(p)-2 {
-// 			lastIndex := pIndex + 2
-// 			isChange := p[lastIndex] == p[pIndex]
-// 			for lastIndex < len(p) {
-// 				if p[lastIndex] != p[pIndex] {
-// 					lastIndex--
-// 					break
-// 				}
-// 				lastIndex++
-// 			}
-// 			if lastIndex == len(p) {
-// 				lastIndex--
-// 			}
-
-// 			// if change index
-// 			if isChange {
-// 				p = p[:pIndex+1] + string(p[pIndex]) + p[pIndex+2:]
-// 				p = p[:lastIndex] + "*" + p[lastIndex+1:]
-// 				isMultiple = false
-// 				fmt.Println(p)
-// 			}
-// 		}
-
-// 		fmt.Println("isMultiple", isMultiple)
-
-// 		if isMultiple {
-// 			if sIndex > len(s)-1 {
-// 				pIndex += 2
-// 				continue
-// 			}
-// 			// if find a rune uncompared
-// 			if p[pIndex] != '.' && s[sIndex] != p[pIndex] {
-// 				pIndex += 2
-// 			} else {
-// 				sIndex++
-// 			}
-// 		} else {
-// 			if sIndex > len(s)-1 {
-// 				// offset
-// 				if s[len(s)-1] == p[pIndex] && p[pIndex-1] == '*' {
-// 					count := 0
-// 					fixedCount := 0
-// 					i := pIndex - 1
-// 					for i >= 0 {
-// 						if p[i] == '*' {
-// 							i -= 2
-// 						} else {
-// 							if p[i] == p[pIndex] {
-// 								fixedCount++
-// 							} else {
-// 								break
-// 							}
-// 							i--
-// 						}
-// 					}
-// 					for pIndex < len(p) {
-// 						if p[pIndex] != s[len(s)-1] {
-// 							isMatch = false
-// 							fmt.Println("trigger 1")
-// 							break
-// 						}
-
-// 						count++
-
-// 						fmt.Println("@@@@@@@@@@", count, len(s)-fixedCount)
-// 						if count > len(s)-fixedCount {
-// 							isMatch = false
-// 							fmt.Println("trigger 2")
-// 							return isMatch
-// 						}
-
-// 						pIndex++
-// 					}
-// 					continue
-// 				} else {
-// 					isMatch = false
-// 					fmt.Println("trigger 3")
-// 					break
-// 				}
-// 			}
-// 			// if find a rune uncompared
-// 			if p[pIndex] != '.' && s[sIndex] != p[pIndex] {
-// 				isMatch = false
-// 				fmt.Println("trigger 4")
-// 				break
-// 			}
-// 			pIndex++
-// 			sIndex++
-// 		}
-// 	}
-
-// 	if pIndex < len(p) || sIndex < len(s) {
-// 		isMatch = false
-// 	}
-
-// 	return isMatch
-// }
+func counts[S []E, E comparable](s S, k E) int {
+	h := map[E]int{}
+	for _, v := range s {
+		h[v] = h[v] + 1
+	}
+	return h[k]
+}
