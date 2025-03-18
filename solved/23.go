934c44533697dd4edcc2cbdd39a612ff903414bf
Noah <fg5678910@yahoo.com.tw>
2025-03-18 10:17:10 +0800

    23 ok

--- solved/23.go
+++ solved/23.go
@@ -2,14 +2,13 @@ package main
 
 import "fmt"
 
-type ListNode struct {
-	Val  int
-	Next *ListNode
-}
-
 func mergeKLists(lists []*ListNode) *ListNode {
 	ans := &ListNode{}
 	curr := ans
+
+	if len(lists) == 0 || checkAllNil(lists) {
+		return nil
+	}
 
 	for {
 		var mi int = -1
@@ -28,12 +27,26 @@ func mergeKLists(lists []*ListNode) *ListNode {
 		}
 
 		curr.Val = lists[mi].Val
-		curr.Next = &ListNode{}
-		curr = curr.Next
 
 		fmt.Println("lists[mi]", lists[mi].Val, lists[mi].Next)
 		lists[mi] = lists[mi].Next
+
+		if checkAllNil(lists) {
+			break
+		}
+
+		curr.Next = &ListNode{}
+		curr = curr.Next
 	}
 
 	return ans
+}
+
+func checkAllNil(lists []*ListNode) bool {
+	for _, node := range lists {
+		if node != nil {
+			return false
+		}
+	}
+	return true
 }
