d4f214d0d12ad17f8bef47080e0e3e70d93b55f8
Noah <fg5678910@yahoo.com.tw>
2025-03-20 13:21:30 +0800

    543 ok

---
+++ solved/543.go
@@ -0,0 +1,26 @@
+package main
+
+import "math"
+
+// Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func diameterOfBinaryTree(root *TreeNode) int {
+	_, diameter := calcDiameter(root)
+	return diameter
+}
+
+func calcDiameter(node *TreeNode) (int, int) {
+	if node == nil {
+		return 0, 0
+	}
+
+	left, leftMax := calcDiameter(node.Left)
+	right, rightMax := calcDiameter(node.Right)
+
+	return 1 + int(math.Max(float64(left), float64(right))), int(math.Max(float64(left+right), math.Max(float64(leftMax), float64(rightMax))))
+}
